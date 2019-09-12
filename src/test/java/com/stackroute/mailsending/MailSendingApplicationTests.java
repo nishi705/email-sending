e5aafef18a8a82f41637326d1e0d8811892ca0ba
nishi.singh <nishi.singh@cgi.com>
2019-09-12 11:40:38 +0530

    email-sending done

---
+++ src/test/java/com/stackroute/mailsending/MailSendingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackroute.mailsending;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailSendingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
